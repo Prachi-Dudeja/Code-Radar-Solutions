c4ed347f22d0244cf8350ed94421abf79ac741af
Prachi-Dudeja <prachi1576.becse24@chitkara.edu.in>
2025-02-04 17:31:00 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    switch(a):
+    case (>= 90):
+        printf('A');
+        break;
+    case(>=80 && <90):
+        printf('B');
+        break ;
+
+    case(>= 70 && <80):
+    printf('C');
+    break;
+
+    case (>= 60 && <70):
+    printf('D');
+    break;
+
+    default:  
+    return 0;
+}
