46f19ffdfe54478d1eebb9db43b4e429d0b0f373
Prachi-Dudeja <prachi1576.becse24@chitkara.edu.in>
2025-02-05 20:55:01 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,19 +6,17 @@ int main() {
     scanf("%d %d %c",&a,&b,&c);
     switch(c){
         case('+'):
-        printf("%d %d",a + b);
+        printf("%d",a + b);
         break;
         case('-'):
-        printf("%d %d",a - b);
+        printf("%d",a - b);
         break;
         case('*'):
-        printf("%d %d",a * b);
+        printf("%d",a * b);
         break;
         case('/'):
-        printf("%d %d",a / b);
+        printf("%d",a / b);
         break;
-        
-
-            }
+         }
     return 0;
 }
