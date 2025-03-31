a633673b7a41d24f59b5d649a9cc600e41ebf36b
PRACHI DUDEJA <dudejaprachi6@gmail.com>
2025-04-01 00:32:50 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,10 @@
+void generateFibonacci(int n) {
+    int t1 = 0, t2 = 1, nextTerm;
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", t1);  // Print the current term
+        nextTerm = t1 + t2; // Calculate the next term
+        t1 = t2;            // Update the first term
+        t2 = nextTerm;      // Update the second term
+    }
+}
