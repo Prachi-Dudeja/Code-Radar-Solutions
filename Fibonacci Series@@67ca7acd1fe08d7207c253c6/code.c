5b4a6c3a36b5325aee62b628155cad4fc5c72dc6
PRACHI DUDEJA <dudejaprachi6@gmail.com>
2025-04-01 00:35:08 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,4 +1,7 @@
-void generateFibonacci() {
+#include <stdio.h>
+
+// Function to generate Fibonacci sequence
+void generateFibonacci(int n) {
     int t1 = 0, t2 = 1, nextTerm;
 
     for (int i = 1; i <= n; i++) {
@@ -7,4 +10,14 @@ void generateFibonacci() {
         t1 = t2;            // Update the first term
         t2 = nextTerm;      // Update the second term
     }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);  // Input the number of terms
+    generateFibonacci(n);  // Call the function to generate Fibonacci sequence
+
+    return 0;
 }
